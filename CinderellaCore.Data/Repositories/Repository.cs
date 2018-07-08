cf2a65cb71b8a4429ee12f8deb847f8cc07d484f
Dennis D. Silva <dsilva609@gmail.com>
2018-07-08 14:30:18 -0400

    updated footer and styles

--- CinderellaCore.Data/Repositories/Repository.cs
+++ CinderellaCore.Data/Repositories/Repository.cs
@@ -28,6 +28,7 @@ namespace CinderellaCore.Data.Repositories
 
         public void Edit(T entity)
         {
+            _context.Entry(entity).State = EntityState.Modified;
             _context.Set<T>().Update(entity);
             _context.SaveChanges();
         }
