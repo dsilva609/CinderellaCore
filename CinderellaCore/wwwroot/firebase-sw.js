54fc09c920e03b99bae952425953855cc61bde5a
Dennis D. Silva <dsilva609@gmail.com>
2018-09-06 16:29:50 -0400

    added firebase messaging

---
+++ CinderellaCore/wwwroot/firebase-sw.js
@@ -0,0 +1,30 @@
+﻿importScripts('https://www.gstatic.com/firebasejs/5.4.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.4.2/firebase-messaging.js');
+
+var config = {
+	apiKey: "AIzaSyD2hPRsVJUdq4MPQOCHwhRq5aocQq0i4Ss",
+	authDomain: "project-cinderella.firebaseapp.com",
+	databaseURL: "https://project-cinderella.firebaseio.com",
+	projectId: "project-cinderella",
+	storageBucket: "project-cinderella.appspot.com",
+	messagingSenderId: "180792182839"
+};
+firebase.initializeApp(config);
+const messaging = firebase.messaging(); 
+messaging.setBackgroundMessageHandler(function (payload) {
+	new Notification("test");
+//	var data = JSON.parse(payload);
+//	self.registration.showNotification(data.title, {
+//		body: data.body,
+//		icon: data.icon,
+//		click_action: data.click_action,
+//		//time_to_live: data.time_to_live,
+//		//data: data.data,
+//		//tag: data.tag
+//	});
+//	console.log("received");
+});
+//firebase.messaging().onMessage(function (payload) {
+//	console.log('Message received. ', payload);
+//	//firebase.notifications().displayNotification(payload);
+//});
--- CinderellaCore/wwwroot/js/site.js
+++ CinderellaCore/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+async function askForPermissionToReceiveNotifications() {
+	try {
+		const messaging = firebase.messaging();
+		await messaging.requestPermission().then(function() {
+			console.log("permission granted");
+		}).catch(function(err) {
+			console.log("permission failed", err);
+		});
+		const token = messaging.getToken().then(function(token) {
+			console.log('token:', token);
+		});
+		
+
+		return token;
+	} catch (error) {
+		console.error("error " + error);
+	}
+}
